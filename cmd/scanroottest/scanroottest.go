eb9909e2d50f20df7dcfb68e2ea42f98d1aa03b7
peterzandbergen <peter.zandbergen@myhops.com>
2015-12-09 20:34:45 +0100

    Cleaned the command up.

--- cmd/scanroottest/scanroottest.go
+++ cmd/scanroottest/scanroottest.go
@@ -13,104 +13,9 @@ import (
 	"github.com/peterzandbergen/dedup/model"
 )
 
-func TestOne() {
-	// var p = `C:\Users\Peter\Dropbox (Moven)`
-	var p = `C:\Users\Peter\Documents`
-	if s, err := filepath.Abs(p); err == nil {
-		p = s
-	}
 
-	root := &model.ScanRoot{
-		Path: p,
-	}
-
-	st := time.Now()
-	err := dedup.ScanTree(root, -1)
-	et := time.Now()
-
-	d := et.Sub(st)
-	fmt.Printf("Scan took %v to run.\n", d.String())
-
-	if err != nil {
-		fmt.Printf("Error during scan: %s\n", err.Error())
-		return
-	}
-	fmt.Printf("Found %d file under %s.\n", len(root.Files), root.Path)
-
-	// fmt.Print(root.String())
-
-	sorted := model.SortSizeName(root.Files)
-
-	fmt.Print(sorted.String())
-}
-
-var testList = model.FileInfoList{
-	&model.FileInfo{
-		Name: "aaa",
-		Size: 200,
-	},
-	&model.FileInfo{
-		Name: "bbb",
-		Size: 200,
-	},
-	&model.FileInfo{
-		Name: "ccc",
-		Size: 200,
-	},
-	&model.FileInfo{
-		Name: "ddd",
-		Size: 300,
-	},
-	&model.FileInfo{
-		Name: "eee",
-		Size: 300,
-	},
-	&model.FileInfo{
-		Name: "fff",
-		Size: 200,
-	},
-}
-
-func TestTwo() {
-	// sn := model.SortSizeName(testList)
-
-	gn := model.EqualSizes(testList)
-
-	fmt.Printf("%s\n", model.EqualSizeList(gn).String())
-}
-
-// TestThree scans the tree starting in r and produces a list with the
-// files that have the same length.
-func TestThree(scanRoot string, largerThan int64) {
-	var p = scanRoot
-	if s, err := filepath.Abs(p); err == nil {
-		p = s
-	}
-
-	root := &model.ScanRoot{
-		Path: p,
-	}
-
-	var st, et time.Time
-	var d time.Duration
-
-	fmt.Printf("Scanning %s...", root.Path)
-	st = time.Now()
-	dedup.ScanTree(root, largerThan)
-	et = time.Now()
-	d = et.Sub(st)
-	fmt.Printf("done\nScan took %v to run.\n", d.String())
-
-	fmt.Print("done.\nLooking for doubles...")
-	st = time.Now()
-	gn := model.EqualSizes(root.Files)
-	et = time.Now()
-	d = et.Sub(st)
-	fmt.Printf("done\nLooking for doubles took %v to run.\n", d.String())
-	fmt.Printf("\n%s\n", model.EqualSizeList(gn).String())
-}
-
-func TestFour(scanRoot string, largerThan int64) {
+// FindIdenticals searches the root and all subfolders for files with the same hash.
+func FindIdenticals(scanRoot string, largerThan int64) {
 	// Scan the root.
 	var p = scanRoot
 	if s, err := filepath.Abs(p); err == nil {
@@ -140,14 +45,14 @@ func TestFour(scanRoot string, largerThan int64) {
 	fmt.Printf("done\nLooking for doubles took %v to run.\n", d.String())
 
 	// Generate the hashes for the equal sized files.
-	HashEquals(gn)
+	hashEquals(gn)
 
-	// Again search for equal files HashEquals.
+	// Again search for equal files hashEquals.
 	eh := model.EqualHashes(root.Files)
 	fmt.Printf("Equal Hashes: \n%s\n", model.EqualSizeList(eh).String())
 }
 
-func HashEquals(equals []model.EqualSize) {
+func hashEquals(equals []model.EqualSize) {
 	h := sha1.New()
 	for _, es := range equals {
 		for _, fs := range es.Files {
@@ -169,5 +74,5 @@ var (
 
 func main() {
 	flag.Parse()
-	TestFour(*scanRoot, *largerThan)
+	FindIdenticals(*scanRoot, *largerThan)
 }
